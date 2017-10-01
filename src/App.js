0a003268dca689e8e74ee529ec14af183c8a5aee
nilesh <nileshgulia@gmail.com>
2017-10-02 00:11:26 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,36 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+      <nav className="navbar navbar-default">
+  <div className="container-fluid">
+
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand" href="#">Awesome Recipe</a>
+    </div>
+
+    
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav">
+        <li className="active"><a href="#">Home <span className="sr-only">(current)</span></a></li>
+        <li><a href="#">Submit a Recipe</a></li>
+        
+      </ul>
+     
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+        
       </div>
     );
   }
