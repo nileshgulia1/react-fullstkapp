19015c274de217f66ec87e7850b19b4042289266
nilesh <nileshgulia@gmail.com>
2017-10-06 22:36:58 +0530

    added form

--- src/Submit.js
+++ src/Submit.js
@@ -14,11 +14,31 @@ export default class Submit extends Component{
        this.props.history.push('/');
    }
     render(){
-        return (<div>
-            <h1>
-            Submit</h1>
-            <button onClick={this.submitRecipe}>Submit a Recipe</button>
-            </div>
+        return (
+        <div className="row">
+         <div className="col-xs-12 col-sm-12">
+            <h1>Submit</h1>
+           <form>
+             <div className="form-group">
+              <label for="name">Name</label>
+               <input type="text" className="form-control" id="name"  placeholder="Enter the name of the recipe" />
+                
+             </div>
+  
+  
+  
+  <div className="form-group">
+    <label for="exampleTextarea">Description</label>
+    <textarea className="form-control" id="exampleTextarea" rows="3"></textarea>
+  </div>
+  
+  
+  
+  <button type="submit" onClick={this.submitRecipe} className="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
+            
 
         );
     }
